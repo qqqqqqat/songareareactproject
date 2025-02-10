4412325b25e0b9ebc0a4abf4309bd7f1c3c040f1
qqqqqqat <65070203@kmitl.ac.th>
2025-02-10 10:52:28 +0700

    delete \*

--- src/app-item.js
+++ src/app-item.js
@@ -6,11 +6,10 @@ function AppItem(props){
         <div className="item" >
           <img alt="" src={itemobj.imgsong} onClick={() => {event(itemobj)}}/>
           <p>{itemobj.titlesong}</p>
-          <iframe title="" src={itemobj.youtube}
+          <iframe title="clip" src={itemobj.youtube}
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen></iframe>
         </div>
     )
 }
-
 export default AppItem
